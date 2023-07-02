3a69b25fe2075430cb4fa0e6dfcf26d2d2b3ec8d
Piotr Kałużny <piotrkaluzny@MacBook-Pro-Piotr.local>
2023-07-02 20:41:12 +0200

    add mui and about page

---
+++ src/pages/about/about.tsx
@@ -0,0 +1,13 @@
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
+export default function About() {
+    return (
+    <>
+    <Box sx={{ display: 'flex' }}>
+    <CircularProgress />
+    </Box>
+    <div>About</div>
+    </>
+    )
+  }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import Link from 'next/link'
 
 const Paragraph = styled.p`
   font-size: 30px;
@@ -9,6 +10,9 @@ export default function Home() {
   return (
     <div >
       <Paragraph>Hello, Emotion!</Paragraph>
+      <li>
+        <Link href="/about">About Us</Link>
+      </li>
     </div>
   );
 }

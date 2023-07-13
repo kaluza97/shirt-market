4251d7f4ba8d252f2a9cba15a3c524e4737cd622
Piotr Kałużny <piotrkaluzny@MacBook-Pro-Piotr.local>
2023-07-13 15:50:19 +0200

    add eslint rulles and fix warnings

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -31,7 +31,7 @@ const AuthProvider: FC<AuthProviderProps> = ({ children }) => {
       }
     });
     return () => unsubscribe();
-  }, [router, auth]);
+  }, [router]);
 
   const login = async (auth: Auth, email: string, password: string) => {
     try {

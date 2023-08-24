213734ccab6ab91c1666d1b3609ca29ef815a62d
Piotr Kałużny <piotrkaluzny@MacBook-Pro-Piotr.local>
2023-08-24 22:23:30 +0200

    refactor: moving link to constants

--- src/components/Terms/Terms.component.tsx
+++ src/components/Terms/Terms.component.tsx
@@ -1,12 +1,13 @@
 import { FC } from 'react';
 import { Checkbox, FormControlLabel } from '@mui/material';
 import { LinkStyled, TermsContainer } from '@/components/Terms/Terms.styles';
+import { privacyPolicyLink } from '@/constants/links';
 
 export const Terms: FC = () => {
   const terms = () => (
     <TermsContainer>
       <p>This is an example of</p>
-      <LinkStyled href="https://www.termsfeed.com/blog/sample-terms-and-conditions-template/">
+      <LinkStyled href={privacyPolicyLink}>
         terms
       </LinkStyled>
     </TermsContainer>

2d3cdf1d140fbcb7228d810adb7082365efe4b2c
Piotr Kałużny <piotrkaluzny@MacBook-Pro-Piotr.local>
2023-09-06 17:21:25 +0200

    minor corrections

--- src/components/Cart/Cart.utils.ts
+++ src/components/Cart/Cart.utils.ts
@@ -1,6 +1,6 @@
 import { CartItem } from '@/redux/slices/Cart/Cart.types';
 
-export const calculateTotalCost = (cartItems: CartItem[]): number => {
+export const calculateTotalCost = (cartItems: Array<CartItem>): number => {
   let totalCost = 0;
 
   cartItems.forEach((item) => {

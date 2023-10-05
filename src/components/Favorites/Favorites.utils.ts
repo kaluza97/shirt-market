cc899de28c1270336a0756c114aff5d0cd6a0dfc
Piotr Kałużny <piotrkaluzny@MacBook-Pro-Piotr.local>
2023-10-05 13:26:40 +0200

    small refactor

--- src/components/Favorites/Favorites.utils.ts
+++ src/components/Favorites/Favorites.utils.ts
@@ -2,7 +2,7 @@ import { FavoriteItem } from '@/redux/slices/Favorites/Favorites.types';
 
 export const calculateIsFavorite = (
   productId: number,
-  favorites: FavoriteItem[] | null
+  favorites: Array<FavoriteItem> | null
 ) => {
   return favorites?.some((favorite) => favorite.id === productId);
 };
